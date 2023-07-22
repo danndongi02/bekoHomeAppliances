adee7ae2ec30119ce9f0a251dc9ac6f7bd50f100
danndongi02 <ianndunguh@gmail.com>
2023-07-22 22:23:07 +0300

    Added product detail page

--- bekoHomeAppliances/urls.py
+++ bekoHomeAppliances/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 
 from core.views import homepage, shop
+from product.views import product
 
 urlpatterns = [
     path("", homepage, name="homepage"),
     path("shop/", shop, name="shop"),
+    path("product/", product, name="product"),
     path('admin/', admin.site.urls),
 ]
