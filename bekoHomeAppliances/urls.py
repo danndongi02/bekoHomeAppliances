1ca07a920ec9b63f5f7cc4f4b9168db1531639a3
danndongi02 <ianndunguh@gmail.com>
2023-07-22 22:10:14 +0300

    Added shop page

--- bekoHomeAppliances/urls.py
+++ bekoHomeAppliances/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import homepage
+from core.views import homepage, shop
 
 urlpatterns = [
     path("", homepage, name="homepage"),
+    path("shop/", shop, name="shop"),
     path('admin/', admin.site.urls),
 ]
--- core/views.py
+++ core/views.py
@@ -9,3 +9,11 @@ def homepage(request):
         'products': products
     }
     return render(request, 'core/homepage.html', context)
+
+def shop(request):
+    products = Product.objects.all()
+    
+    context = {
+        'products': products
+    }
+    return render(request, 'core/shop.html', context)
