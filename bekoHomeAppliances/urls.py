8223441cfb6c19c1088bb87862c24abe1c91665f
danndongi02 <ianndunguh@gmail.com>
2023-07-27 02:31:37 +0300

    Added cart bubble and adding to cart logic

--- bekoHomeAppliances/urls.py
+++ bekoHomeAppliances/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 
 from core.views import homepage, shop
 from product.views import product
+from cart.views import add_to_cart
 
 urlpatterns = [
     path("", homepage, name="homepage"),
     path("shop/", shop, name="shop"),
     path("shop/<slug:slug>/", product, name="product"),
     path('admin/', admin.site.urls),
+    path("add_to_cart/<int:product_id>/", add_to_cart, name="add_to_cart")
 ]
